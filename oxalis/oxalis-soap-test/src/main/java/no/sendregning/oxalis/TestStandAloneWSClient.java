d62d20570e62a010f59a33dc361ebfda408e06de
Nigel Parker <nigel.parker@mazeppa.no>
2011-10-25 10:53:07 +0200

    Removed TestDaemon (no longer used)

--- oxalis/oxalis-soap-test/src/main/java/no/sendregning/oxalis/TestStandAloneWSClient.java
+++ oxalis/oxalis-soap-test/src/main/java/no/sendregning/oxalis/TestStandAloneWSClient.java
@@ -1,11 +1,32 @@
 package no.sendregning.oxalis;
 
+import eu.peppol.outbound.api.DocumentSender;
+import eu.peppol.outbound.api.DocumentSenderBuilder;
+import eu.peppol.start.util.Configuration;
+
+import java.io.File;
+import java.net.URL;
+
 /**
  * User: ravnholt
  */
 public class TestStandAloneWSClient {
 
     public static void main(String[] args) throws Exception {
-        new TestDaemon().run();
+        Configuration configuration = Configuration.getInstance();
+
+        File xmlInvoice = new File(configuration.getProperty("test.file"));
+        String recipient = "9909:976098897";
+        URL destination = new URL(configuration.getProperty("web.service.address"));
+
+        DocumentSender documentSender = new DocumentSenderBuilder()
+                .setKeystoreFile(new File(configuration.getProperty("keystore")))
+                .setKeystorePassword(configuration.getProperty("keystore.password"))
+                //.enableSoapLogging()
+                .build();
+
+        documentSender.sendInvoice(xmlInvoice, recipient, recipient, destination);
+
+        Log.info("Test message successfully dispatched");
     }
 }
