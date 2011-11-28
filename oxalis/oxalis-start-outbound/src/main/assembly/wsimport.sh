b4b192951acf1c8b530630103f581fd8a3da440b
Steinar Cook <steinar@sendregning.no>
2011-11-28 11:15:05 +0100

    Modified the start-inbound and start-outbound to use the jaxws-maven-plugin rather than exec-maven-plugin for execution of wsimport, i.e. generation of code from .wsdl files. This should make things work on Windows and other plattforms.

--- oxalis/oxalis-start-outbound/src/main/assembly/wsimport.sh
+++ oxalis/oxalis-start-outbound/src/main/assembly/wsimport.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-
+#
+# Executes the wsimport utility
+#
+# Invoked by maven-exec-plugin in start-inbound and start-outbound.
+# This utility will be phased out, once the jaxws-maven-plugin works as expected.
+#
 if [ ! -d $1 ];
 then
     mkdir -p $1
     mkdir -p $2
+    echo wsimport -s $1 -d $2 -p $3 $4 -extension
     wsimport -s $1 -d $2 -p $3 $4 -extension
 else
     echo "Wsdl sources already generated"
