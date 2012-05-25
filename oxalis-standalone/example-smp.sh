b2e36744da98a17486d8220e825af840be2eccb0
Steinar Cook <steinar@sendregning.no>
2012-05-25 13:10:28 +0200

    fixed the sample shell scripts

--- oxalis-standalone/example-smp.sh
+++ oxalis-standalone/example-smp.sh
@@ -29,21 +29,35 @@ if [ ! -r "$KEYSTORE" ]; then
 fi
 
 # If another receiver was supplied on the command line, use it.
-if [ $# = 1 ]
+if [ $# -ge 1 ]
 then
 	export RECEIVER="$1"
 fi
 
+# If another file was specified on the command line, use it.
+if [ $# -ge 2 ]
+then
+    export SAMPLE_FILE=$2
+    if [ ! -r "$SAMPLE_FILE" ]; then
+        echo "$SAMPLE_FILE can not be read"
+        exit 4
+    fi
+fi
 
-
+#
+# Sends the file using a Document type identificator indicating a Peppol bis4a document (generic european)
+# and a process type identificator indicating CEN/BII "Invoice only"
+# This is the simplest of them all.
+#
 java -jar $INSTALL_DIR/target/oxalis-standalone.jar \
 -kf $KEYSTORE \
 -kp=peppol \
 -f $SAMPLE_FILE \
--p INVOICE_ONLY \
+-d "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:www.cenbii.eu:transaction:biicoretrdm010:ver1.0:#urn:www.peppol.eu:bis:peppol4a:ver1.0::2.0" \
+-p "urn:www.cenbii.eu:profile:bii04:ver1.0" \
 -c CH1 \
 -r $RECEIVER \
--s 9908:976098897 
+-s 9908:976098897
 
 # Other usefull PPIDs:
 # SendRegning
@@ -51,4 +65,4 @@ java -jar $INSTALL_DIR/target/oxalis-standalone.jar \
 #
 # ESV = 0088:7314455667781
 # EdiGard 9908:994241257
-#  
+#
--- oxalis-standalone/example.sh
+++ oxalis-standalone/example.sh
@@ -8,7 +8,8 @@ java -jar target/oxalis-standalone.jar \
 -kf ~/appl/apache-tomcat-7.0.22/conf/keystore/keystore.jks \
 -kp=peppol \
 -f /Users/steinar/Dropbox/SendRegning/bussinessdevelopment/PEPPOL/EHF/ehf-test.xml \
--p PROCUREMENT \
+-d "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:www.cenbii.eu:transaction:biicoretrdm010:ver1.0:#urn:www.peppol.eu:bis:peppol4a:ver1.0::2.0" \
+-p "urn:www.cenbii.eu:profile:bii04:ver1.0" \
 -c CH1 \
 -r 9908:976098897 \
 -s 9908:976098897 \
