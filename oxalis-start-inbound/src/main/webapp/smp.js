f0a2a7dcaa5046ac27d43d62e3b56010538bf566
steinarcook <steinar@sendregning.no>
2012-11-09 15:52:11 +0100

    SRAP-181 - Refactor the use of Acronyms for PeppolDocumentTypeIdentifiers (PeppolDocumentId, PeppolProcessId)

---
+++ oxalis-start-inbound/src/main/webapp/smp.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: steinar
+ * Date: 08.11.12
+ * Time: 20:43
+ */
+
+var smp = (function () {
+    "use strict";
+
+    function hello() {
+        alert("Hello");
+    }
+
+    return {
+        hello : hello
+    };
+
+}());
+
+
