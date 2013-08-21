8b4468d6140324ef968fd9c2efa681e98b6abac3
Erlend Klakegg Bergheim <erlend@klakegg.net>
2013-08-21 08:32:13 +0200

    * Allow SQL-script to be rerun without losing data.

--- oxalis-sql/src/main/resources/sql/raw_stats-mysql.sql
+++ oxalis-sql/src/main/resources/sql/raw_stats-mysql.sql
@@ -1,6 +1,4 @@
-drop table if exists raw_stats;
-
-create table raw_stats(
+create table if not exists raw_stats(
   id integer auto_increment primary key,
   ap varchar(35) not null,
   tstamp timestamp default current_timestamp,
