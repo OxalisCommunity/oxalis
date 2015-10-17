72e0b135a49e498414eb427b1540bf0c147cb4ba
Steinar Overbeck Cook <steinar@sendregning.no>
2015-10-17 14:21:40 +0200

    Improved the bash script smp.sh
     1. Allow searching for document types with a regexp
     1. Added -t option to allow for Test SMP usage (SMK)

--- oxalis-standalone/src/main/bash/smp.sh
+++ oxalis-standalone/src/main/bash/smp.sh
@@ -14,10 +14,16 @@
 #
 
 declare -r GET_SERVICE_GROUP="GROUP"
-declare -r GET_SERVICE_META="SERVICE"
+declare -r GET_DOC_TYPE_INFO="DOC_TYPE_INFO"
 
-declare -r SML=".iso6523-actorid-upis.edelivery.tech.ec.europa.eu"
-declare -r SMK=".iso6523-actorid-upis.acc.edelivery.tech.ec.europa.eu"
+declare -r PRODUCTION_SML=".iso6523-actorid-upis.edelivery.tech.ec.europa.eu"
+declare -r TEST_SML=".iso6523-actorid-upis.acc.edelivery.tech.ec.europa.eu"
+
+# Difi test participant id
+declare -r DIFI_TEST_PPID="9908:810418052"
+
+# Default behaviour uses the production SML, may be changed with the -t option
+declare SML=${PRODUCTION_SML}
 
 # Holds the current option letter
 declare opt=""
@@ -25,9 +31,22 @@ declare PEPPOL_ID=""
 
 function usage {
     cat <<-EOH
+
+    Usage: $0 -p participant_id [-g] [-d doc_type] [-t]
+
     -p <peppol participant id> Participant ID (required)
-    -g                     Fetch list of service groups
-    -d <document_type_id>  Fetch service meta data for given document type (TODO)
+    -g Fetch list of service groups
+    -d <document_type_id>  Fetch service meta data for given document type, which may be specified as a regexp
+    -t Uses the test SML rather than the production SML
+
+    Sample Participant ID (Difi Test): $DIFI_TEST_PPID
+
+    Test SML (SMK): $TEST_SML
+
+    Example: Does Difi support reception of electronic Tenders in TEST mode?
+
+    $0 -p $DIFI_TEST_PPID -d Tender -t
+
 EOH
 }
 
@@ -39,6 +58,13 @@ function hostnameForPEPPOL_ID() {
     echo $hash
 }
 
+#
+# Retrieves a complete list of all the capabilities supported by the supplied PPID.
+# The list simply contains all the URLs referencing the complete data set for
+# each document type wrapped in XML
+#
+# Returns an XML formated list
+#
 function serviceGroups() {
     hash_id=$1
 
@@ -51,6 +77,21 @@ function serviceGroups() {
     echo $service_group_data | xmllint --format -
 }
 
+#
+# Retrieves the complete list of all capabilities supported and extracts the URL only
+#
+# Returns a list of URLs
+#
+function service_metadata_url_list() {
+        service_groups=`serviceGroups "$HASH"`
+        href_list=$(echo $service_groups | xpath "//ns2:ServiceMetadataReference/@href" 2>/dev/null |\
+        sed -e 's/href="/\
+        /g' -e 's/"//g' )
+
+        echo ${href_list}
+}
+
+# TODO: make this wretched function actually work :-)
 function lookup_doctype() {
     hash=$1
     doctype=$2
@@ -65,7 +106,7 @@ function lookup_doctype() {
 }
 
 
-urldecode() {
+function urldecode() {
     arg="$1"
     i="0"
     while [ "$i" -lt ${#arg} ]; do
@@ -82,7 +123,7 @@ urldecode() {
     done
 }
 
-urlencode() {
+function urlencode() {
   local string="${1}"
   local strlen=${#string}
   local encoded=""
@@ -98,10 +139,12 @@ urlencode() {
   echo "${encoded}"    # You can either set a return variable (FASTER)
   REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
 }
+
+
 #
 #     M A I N
 #
-while getopts "p:gd:" opt
+while getopts "p:gd:t" opt
 do
     case "$opt" in
         p)
@@ -112,8 +155,10 @@ do
             MODE=$GET_SERVICE_GROUP
             ;;
 
-        d)  MODE=$GET_SERVICE_META
+        d)  MODE=$GET_DOC_TYPE_INFO
             DOC_TYPE="$OPTARG"
+            ;;
+        t)  SML=${TEST_SML}
             ;;
         :)
             echo "Option -$OPTARG requires an argument" >&2
@@ -129,6 +174,11 @@ done
 # Calculates the hostname by hashing the PEPPOL Participant Id
 HASH=`hostnameForPEPPOL_ID "$PEPPOL_ID"`
 
+if [ "$SML" == "$PRODUCTION_SML" ]; then
+    echo "Using production SML: $PRODUCTION_SML"
+else
+    echo "Using test SML: $TEST_SML"
+fi
 
 case "$MODE" in
 
@@ -137,17 +187,29 @@ case "$MODE" in
     # to stdout.
     #
     $GET_SERVICE_GROUP)
-        service_groups=`serviceGroups "$HASH"`
-        url_list=$(echo $service_groups | xpath "//ns2:ServiceMetadataReference/@href" 2>/dev/null |\
-        sed -e 's/href="/\
-        /g' -e 's/"//g' )
+        url_list=$(service_metadata_url_list)
 
         for url in $url_list; do
+            echo "-------------------------------------------------"
+            echo $url
             curl $url 2>/dev/null | xmllint --format - |egrep "(DocumentIdentifier)|(ProcessIdentifier)"
-       done | sed -e 's/^.*<.*>urn/urn/g' -e 's/<\/.*>//g'
+        done | sed -e 's/^.*<.*>urn/urn/g' -e 's/<\/.*>//g'
       ;;
 
-    $GET_SERVICE_META)
-        lookup_doctype  "$HASH" "$DOC_TYPE"
+    $GET_DOC_TYPE_INFO)
+        # Retrieves list of all supported documents in all processes with all the protocols supported
+        url_list=$(service_metadata_url_list)
+        found=0
+        for url in $url_list; do
+            if [[ "$url" =~ $DOC_TYPE ]]; then
+                echo $url
+                curl $url 2>/dev/null | xmllint --format -
+                found=1
+            fi
+        done
+        if [[ "$found" == "0" ]]; then
+            echo "No matching entries found for $DOC_TYPE"
+            exit 4
+        fi
     ;;
 esac
