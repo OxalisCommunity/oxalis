bb1579e5da85f14da17769d34fa5f27fe351b91f
Steinar Overbeck Cook <steinar.cook@gmail.com>
2016-11-07 09:20:28 +0100

    Introduced DbmsPlatform to handle various SQL dialects.

---
+++ oxalis-persistence/src/main/java/eu/peppol/persistence/jdbc/platform/MySqlPlatform.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010 - 2016 Norwegian Agency for Public Government and eGovernment (Difi)
+ *
+ * This file is part of Oxalis.
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission
+ * - subsequent versions of the EUPL (the "Licence"); You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl5
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the Licence
+ *  is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Licence for the specific language governing permissions and limitations under the Licence.
+ *
+ */
+
+package eu.peppol.persistence.jdbc.platform;
+
+/**
+ * @author steinar
+ *         Date: 06.11.2016
+ *         Time: 17.30
+ */
+ class MySqlPlatform extends AbstractDbmsPlatform implements DbmsPlatform {
+
+    public MySqlPlatform(String databaseProductName) {
+        super(databaseProductName);
+    }
+
+    @Override
+    public String getLimitClause(int offset, int rowCount) {
+        return "LIMIT " + rowCount + " OFFSET " + offset;
+    }
+}
---
+++ oxalis-persistence/src/test/java/eu/peppol/persistence/jdbc/platform/H2DatabasePlatformTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2010 - 2016 Norwegian Agency for Public Government and eGovernment (Difi)
+ *
+ * This file is part of Oxalis.
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission
+ * - subsequent versions of the EUPL (the "Licence"); You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl5
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the Licence
+ *  is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Licence for the specific language governing permissions and limitations under the Licence.
+ *
+ */
+
+package eu.peppol.persistence.jdbc.platform;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author steinar
+ *         Date: 06.11.2016
+ *         Time: 21.21
+ */
+public class H2DatabasePlatformTest {
+
+    @Test
+    public void testLimit() {
+        H2DatabasePlatform h2 = new H2DatabasePlatform("H2");
+        String limitClause = h2.getLimitClause(10, 20);
+        assertEquals(limitClause, "LIMIT 20 OFFSET 10");
+
+    }
+}
