c0dc41a05aa5f05694ce4ebac13e46b7be736579
Steinar Overbeck Cook <steinar.cook@gmail.com>
2017-01-01 12:39:46 +0100

    Added a helpfull shell script

---
+++ oxalis-standalone/src/main/bash/send-self.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#
+# Sends sample invoice to ourselves
+#
+
+PRG="$0"
+
+# Name of directory from which this program is being executed, should normally result in the bin/ directory
+# of the distribution.
+PRGDIR=`dirname $0`
+
+
+RECEIVER="9908:983995691"
+
+# PEPPOL Participant id for Difi
+DIFI_TEST="9908:810418052"
+
+#
+# Allows overriding from command line
+#
+for i in "$@"
+do
+	case $i in
+		-r=*)
+		RECEIVER="${i#*=}"
+		shift
+		;;
+	esac
+done
+
+echo "Sender eksempel faktura (bare tull) fra og til oss selv"
+
+JARFILE=$PRGDIR/oxalis-standalone.jar
+
+if [ ! -r "$JARFILE" ]; then
+	echo "$JARFILE not found"
+	exit 4
+fi
+
+java -jar $JARFILE -f ./sample-invoice.xml -s 9908:976098897 -r ${RECEIVER} -u http://localhost:8080/oxalis/as2 -m as2 -i APP_1000000270 -e data/evidence
