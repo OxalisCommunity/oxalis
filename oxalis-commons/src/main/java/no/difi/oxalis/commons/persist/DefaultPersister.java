e693d0ed2285c87f0dc1260f90908a9145e6f304
Steinar Overbeck Cook <steinar.cook@gmail.com>
2017-01-25 10:55:17 +0100

    Slight improvement to the DefaultPersister

--- oxalis-commons/src/main/java/no/difi/oxalis/commons/persist/DefaultPersister.java
+++ oxalis-commons/src/main/java/no/difi/oxalis/commons/persist/DefaultPersister.java
@@ -6,6 +6,8 @@ import no.difi.oxalis.api.inbound.InboundMetadata;
 import no.difi.oxalis.api.persist.PayloadPersister;
 import no.difi.oxalis.api.persist.ReceiptPersister;
 import no.difi.vefa.peppol.common.model.Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,25 +22,35 @@ import java.nio.file.Paths;
  */
 public class DefaultPersister implements PayloadPersister, ReceiptPersister {
 
+    public static final Logger log = LoggerFactory.getLogger(DefaultPersister.class);
+    private final String tmpDir;
+
+    public DefaultPersister() {
+        tmpDir = System.getProperty("java.io.tmpdir");
+
+    }
+
     @Override
     public Path persist(MessageId messageId, Header header, InputStream inputStream) throws IOException {
+
         Path folder = Paths.get(
+                tmpDir,
                 "inbound",
                 header.getReceiver().getIdentifier(),
                 header.getSender().getIdentifier());
 
         Files.createDirectories(folder);
 
-        Path path = Paths.get(
-                "inbound",
-                header.getReceiver().getIdentifier(),
-                header.getSender().getIdentifier(),
+        
+        Path path = Paths.get(folder.toString(),
                 String.format("%s.xml", messageId.stringValue()));
 
         try (OutputStream outputStream = Files.newOutputStream(path)) {
             ByteStreams.copy(inputStream, outputStream);
         }
 
+        log.debug("Payload persisted to: " + path);
+        
         return path;
     }
 
