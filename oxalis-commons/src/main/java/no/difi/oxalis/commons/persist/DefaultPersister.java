6c9413893bc39d1e86e32be1dc3c567bba7096ea
Steinar Overbeck Cook <steinar.cook@gmail.com>
2017-01-25 21:38:15 +0100

    Removed integration test which expected evidence data to be written to the database

--- oxalis-commons/src/main/java/no/difi/oxalis/commons/persist/DefaultPersister.java
+++ oxalis-commons/src/main/java/no/difi/oxalis/commons/persist/DefaultPersister.java
@@ -1,25 +1,3 @@
-/*
- * Copyright 2010-2017 Norwegian Agency for Public Management and eGovernment (Difi)
- *
- * Licensed under the EUPL, Version 1.1 or – as soon they
- * will be approved by the European Commission - subsequent
- * versions of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- *
- * You may obtain a copy of the Licence at:
- *
- * https://joinup.ec.europa.eu/community/eupl/og_page/eupl
- *
- * Unless required by applicable law or agreed to in
- * writing, software distributed under the Licence is
- * distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied.
- * See the Licence for the specific language governing
- * permissions and limitations under the Licence.
- */
-
 package no.difi.oxalis.commons.persist;
 
 import com.google.common.io.ByteStreams;
@@ -27,7 +5,6 @@ import eu.peppol.identifier.MessageId;
 import no.difi.oxalis.api.inbound.InboundMetadata;
 import no.difi.oxalis.api.persist.PayloadPersister;
 import no.difi.oxalis.api.persist.ReceiptPersister;
-import no.difi.oxalis.commons.filesystem.FileUtils;
 import no.difi.vefa.peppol.common.model.Header;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,27 +23,33 @@ import java.nio.file.Paths;
 public class DefaultPersister implements PayloadPersister, ReceiptPersister {
 
     public static final Logger log = LoggerFactory.getLogger(DefaultPersister.class);
+    private final String tmpDir;
+
+    public DefaultPersister() {
+        tmpDir = System.getProperty("java.io.tmpdir");
+
+    }
 
     @Override
     public Path persist(MessageId messageId, Header header, InputStream inputStream) throws IOException {
+
         Path folder = Paths.get(
+                tmpDir,
                 "inbound",
-                FileUtils.filterString(header.getReceiver().getIdentifier()),
-                FileUtils.filterString(header.getSender().getIdentifier()));
+                header.getReceiver().getIdentifier(),
+                header.getSender().getIdentifier());
 
         Files.createDirectories(folder);
 
-        Path path = Paths.get(
-                "inbound",
-                FileUtils.filterString(header.getReceiver().getIdentifier()),
-                FileUtils.filterString(header.getSender().getIdentifier()),
-                String.format("%s.xml", FileUtils.filterString(messageId.stringValue())));
+
+        Path path = Paths.get(folder.toString(),
+                String.format("%s.xml", messageId.stringValue()));
 
         try (OutputStream outputStream = Files.newOutputStream(path)) {
             ByteStreams.copy(inputStream, outputStream);
         }
 
-        log.debug("Payload persisted to '{}'.", path);
+        log.debug("Payload persisted to: " + path);
 
         return path;
     }
