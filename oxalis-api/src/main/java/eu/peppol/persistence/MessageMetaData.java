dec679cdebbbe3da5c38a877e4a2a48e7fa5762c
Erlend Klakegg Bergheim <erlend@klakegg.net>
2017-01-25 18:38:32 +0100

    Updating copyright statement.

--- oxalis-api/src/main/java/eu/peppol/persistence/MessageMetaData.java
+++ oxalis-api/src/main/java/eu/peppol/persistence/MessageMetaData.java
@@ -1,19 +1,23 @@
 /*
- * Copyright (c) 2010 - 2016 Norwegian Agency for Public Government and eGovernment (Difi)
+ * Copyright 2010-2017 Norwegian Agency for Public Management and eGovernment (Difi)
  *
- * This file is part of Oxalis.
+ * Licensed under the EUPL, Version 1.1 or – as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the European Commission
- * - subsequent versions of the EUPL (the "Licence"); You may not use this work except in compliance with the Licence.
+ * You may not use this work except in compliance with the Licence.
  *
  * You may obtain a copy of the Licence at:
  *
- * https://joinup.ec.europa.eu/software/page/eupl5
+ * https://joinup.ec.europa.eu/community/eupl/og_page/eupl
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the Licence
- *  is distributed on an "AS IS" basis,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Licence for the specific language governing permissions and limitations under the Licence.
- *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
  */
 
 package eu.peppol.persistence;
