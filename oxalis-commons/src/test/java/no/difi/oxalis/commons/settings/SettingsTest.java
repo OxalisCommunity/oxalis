0f9fea41ab4184a02920b4ae86cf7c936ddffda2
Erlend Klakegg Bergheim <erlend@klakegg.net>
2017-02-11 19:43:17 +0100

    Adding some tests.

---
+++ oxalis-commons/src/test/java/no/difi/oxalis/commons/settings/SettingsTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2010-2017 Norwegian Agency for Public Management and eGovernment (Difi)
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ *
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/community/eupl/og_page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+
+package no.difi.oxalis.commons.settings;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.util.Types;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import no.difi.oxalis.api.lang.OxalisLoadingException;
+import no.difi.oxalis.api.settings.Settings;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author erlend
+ */
+public class SettingsTest {
+
+    private Settings<TestConf> settings;
+
+    @BeforeClass
+    @SuppressWarnings("unchecked")
+    public void beforeClass() {
+        Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                SettingsBuilder.with(binder(), TestConf.class);
+
+                bind(Config.class).toInstance(ConfigFactory.parseMap(ImmutableMap.of("test.value", "200")));
+            }
+        });
+
+        settings = injector.getInstance((Key<Settings<TestConf>>)
+                Key.get(Types.newParameterizedType(Settings.class, TestConf.class)));
+    }
+
+    @Test
+    public void simple() {
+        Assert.assertEquals(settings.getInt(TestConf.WITH_VALUE), 200);
+        Assert.assertEquals(settings.getString(TestConf.WITH_DEFAULT), "Test");
+        Assert.assertNull(settings.getString(TestConf.WITH_NULLABLE));
+        Assert.assertEquals("Test", settings.getNamed(TestConf.WITH_DEFAULT).value());
+    }
+}
