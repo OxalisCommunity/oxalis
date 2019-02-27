5ebf86bc25ffbd22d2d7bf0fe003a19f9430ce1a
Erlend Klakegg Bergheim <erlend@klakegg.net>
2019-02-27 15:04:19 +0100

    Revert "Adding use of "exec" in Docker image."
    This reverts commit 08f35f73175fcd6bff014e8e6964ccdb3b2e6f69.

--- oxalis-dist/oxalis-server/src/main/assembly/full/bin/run.sh
+++ oxalis-dist/oxalis-server/src/main/assembly/full/bin/run.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $(readlink -f $0))/..
 
-exec java $JAVA_OPTS -classpath conf/*:lib/*:ext/* no.difi.oxalis.server.Main $@
+java $JAVA_OPTS -classpath conf/*:lib/*:ext/* no.difi.oxalis.server.Main $@
